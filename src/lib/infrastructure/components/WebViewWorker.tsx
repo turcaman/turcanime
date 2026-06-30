ef337bdc639714aae4f0fc77c4f39d6cba82719a
Turca Man <280507438+turcaman@users.noreply.github.com>
2026-06-29 18:20:56 -0600

    chore: final cleanup - remove empty dirs, merge injectionScripts
    - Remove empty directories (version/, metrics/, strategies/)
    - Merge injectionScripts.ts into WebViewWorker.tsx
    - Delete injectionScripts.ts (now inlined)
    - Keep storage.ts as shared utility (used by 3 stores)

--- src/lib/infrastructure/components/WebViewWorker.tsx
+++ src/lib/infrastructure/components/WebViewWorker.tsx
@@ -6,7 +6,30 @@ import type { ISession } from "../../domain/interfaces";
 import { logger } from "../../utils/logger";
 import { GLOBAL_BOOTSTRAP } from "../webview/bootstrap";
 import { ANIMELATINO_CONFIG } from "../../config/providerConfigs";
-import { IFRAME_EXTRACT_FN } from "../webview/injectionScripts";
+
+/** Extracts the first iframe src and posts it as EMBED_VIDEO_URL. */
+const IFRAME_EXTRACT_FN = `(function(){
+  try {
+    var f = document.querySelector('iframe[src]');
+    var msg = '[WebView] IFRAME_EXTRACT_FN: found iframe? ' + (!!f) + ' ' + (f && f.src);
+    console.log(msg);
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({type:'LOG',data:msg}));
+    }
+    if (f && f.src && window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({
+        type: 'EMBED_VIDEO_URL',
+        data: f.src
+      }));
+    }
+  } catch(e) {
+    var errMsg = '[WebView] IFRAME_EXTRACT_FN error: ' + (e.message || e);
+    console.log(errMsg);
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({type:'LOG',data:errMsg}));
+    }
+  }
+})();`;
 
 const WORKER_URL = ANIMELATINO_CONFIG.sessionWashUrl;
 
